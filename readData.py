49d228e1cf7074ffd245fc7bc55dd4c74e7ceddd
Varun Sashidharan <svarun94@gmail.com>
2015-04-25 10:05:41 +0530

    stripped data

--- readData.py
+++ readData.py
@@ -1,4 +1,7 @@
 import csv
+import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 def read_data():
 	with open("letter.data",'r') as f:
@@ -15,4 +18,18 @@ def read_data():
 					continue
 				pixels.append(int(j))
 			d[char].append(pixels)
+			
 		return d
+
+def strip_data(d):
+	training = {}
+	testing = {}
+	for key in d:
+		training[key]=[]
+		testing[key]=[]
+		training[key].extend(d[key][:32])
+		testing[key].extend(d[key][32:40])
+	return training,testing
+
+data = read_data()
+trng,tsting = strip_data(data)
